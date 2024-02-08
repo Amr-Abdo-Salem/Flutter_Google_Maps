eb824c2490b0b8d0467c135a03c212c8ab03902e
Amr Salem <93786481+Amr-Abdo-Salem@users.noreply.github.com>
2024-02-08 11:09:03 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/pro_google_map/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pro_google_map
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FlutterGoogleMaps());
+}
+
+class FlutterGoogleMaps extends StatelessWidget {
+  const FlutterGoogleMaps({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
