9132c3036f8d26a8c7685b87e52243256779c81e
Amr Salem <93786481+Amr-Abdo-Salem@users.noreply.github.com>
2024-02-13 23:50:03 +0200

    update environment and add live location for app

---
+++ lib/Views/Widgets/live_location_body_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LiveLocationBodyWidget extends StatefulWidget {
+  const LiveLocationBodyWidget({super.key});
+
+  @override
+  State<LiveLocationBodyWidget> createState() => _LiveLocationBodyWidgetState();
+}
+
+class _LiveLocationBodyWidgetState extends State<LiveLocationBodyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ lib/Views/live_location_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:pro_google_map/Views/Widgets/live_location_body_widget.dart';
+
+class LiveLocationView extends StatelessWidget {
+  const LiveLocationView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const LiveLocationBodyWidget();
+  }
+}
